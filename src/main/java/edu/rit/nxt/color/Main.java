a774fdf161aaf1b77c24f01ce14a39d99939e1ce
Victor J. Marin <vxm4964@rit.edu>
2019-01-27 22:26:28 -0500

    Press ENTER to start.

--- src/main/java/edu/rit/nxt/color/Main.java
+++ src/main/java/edu/rit/nxt/color/Main.java
@@ -5,10 +5,14 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.util.Queue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Main {
 
@@ -26,21 +30,45 @@ public class Main {
 		ColorStateLogger logger = new ColorStateLogger(FREQ, deck);
 		logger.logStates(filename + "-color-states.csv");
 
-		// Start presentation.
 		JFrame screen = getScreen();
-		SlideShow ss = new SlideShow(screen, deck);
-		ss.play();
+		JLabel lbl = new JLabel("Press <ENTER> to start.", SwingConstants.CENTER);
+		screen.add(lbl);
+
+		screen.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					screen.remove(lbl);
+
+					// Blank cursor.
+					Toolkit toolkit = Toolkit.getDefaultToolkit();
+					Point hotSpot = new Point(0, 0);
+					BufferedImage cursorImage = new BufferedImage(1, 1, BufferedImage.TRANSLUCENT);
+					Cursor invisibleCursor = toolkit.createCustomCursor(cursorImage, hotSpot, "InvisibleCursor");
+					screen.setCursor(invisibleCursor);
+
+					// Start presentation.
+					SlideShow ss = new SlideShow(screen, deck);
+					ss.play();
+				} else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+					screen.dispose();
+					System.exit(0);
+				}
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+			}
+		});
 	}
 
 	private static JFrame getScreen() {
 		JFrame frame = new JFrame();
-
-		// Blank cursor.
-		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
-		frame.getContentPane().setCursor(blankCursor);
-
-		// Fullscreen.
 		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		gd.setFullScreenWindow(frame);
 		return frame;
